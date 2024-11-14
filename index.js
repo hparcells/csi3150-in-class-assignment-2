31498b5c9b8b9ed26407b11286a45f10820e9a7a
Hunter Parcells <hunterparcells@gmail.com>
2024-11-14 13:47:23 -0500

    feat: One-liner.

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-function show(letter) {
-  const element = document.querySelector(`.infobox-${letter}`);
-  
-  element.classList.remove('hidden');
-}
-
-function hide(letter) {
-  const element = document.querySelector(`.infobox-${letter}`);
-
-  element.classList.add('hidden');
-}
+const set = (letter, hidden) => document.querySelector(`.infobox-${letter}`).classList.toggle('hidden', hidden);
